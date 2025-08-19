8706a54c813097c5a8b06253b2b0a00cc4ba51c7
koreankelin <koreankelin@gmail.com>
2025-08-19 10:44:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ bot.on('photo', async (msg) => {
 
   try {
     // 그룹으로 사진 전송 (토픽 없음)
-    await bot.sendPhoto(TARGET_GROUP, photoId, {
+    await bot.sendPhoto(TARGET_GROUP+"/461", photoId, {
       caption: `📌 MFY: ${selectedAlley}\n👤 User: ${username}\n🕒 Vaqt: ${new Date().toLocaleString()}`
     });
 
